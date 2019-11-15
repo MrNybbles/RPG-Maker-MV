29a22a3aa88e015b01c3845b357a7a01b9f19721
MrNybbles <57555909+MrNybbles@users.noreply.github.com>
2019-11-15 11:34:19 -0700

    Renamed from nyb_OpaqueData.js.
    Also removed 3 console.info() statements and updated the descriptions.

--- nyb_CompressedDataDir.js
+++ nyb_CompressedDataDir.js
@@ -1,13 +1,17 @@
-/* nyb_OpaqueData.js
- * Version: 20191109b
+/* nyb_CompressedDataDir.js
+ * Version: 20191115
 */
 /*:
- * @plugindesc Obfuscates plain-text files in the game's data/ directory.
- * Just run once after Deployment.
+ * @plugindesc Compressed Data Directory.
  *
- * @author Mr.Nybbles
+ * @author MrNybbles
+ * 
+ * @help [Description]
+ *  Run once after Deployment and the data/ directory contents will be compressed.
+ *  Compresses the plain-text .json files of the game's data/ directory using
+ *  Base64 Encoding + RLE Compression. Has a side-effect of obfuscating the data.
  *
- * @help [Instructions]
+ * [Instructions]
  * 1) Add this plugin to your Project and Enable it.
  * 2) In RPG Maker MV select 'File->Deployment'.
  * 3) Export and Encrypt as normal.
@@ -15,28 +19,27 @@
  *    and delete the deployed unencrypted files.
  *
  * [License]
- * MIT https://opensource.org/licenses/MIT
+ *  MIT https://opensource.org/licenses/MIT
  *
- * Copyright 2019 MrNybbles
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ *  Copyright 2019 MrNybbles
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files
+ *  (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge,
+ *  publish, distribute, sublicense, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 'use strict';
 
@@ -48,7 +51,6 @@
 	
 	
 	const encodeFile = function(fileSpec) {
-		console.info('Encoding ' + fileSpec);
 		
 		const src = path.join(dataDir, fileSpec)
 		const dst = src + suffix;
@@ -93,7 +95,6 @@
 		
 		DataManager.loadDatabase = function() {
 			if(!fs.existsSync(path.join(dataDir, this._databaseFiles[0].src + suffix))) {
-				console.info('[Begin Encoding]');
 				
 				fs.readdirSync(dataDir)
 				.filter(entry => entry.endsWith('.json'))
@@ -101,12 +102,11 @@
 					encodeFile.call(this, entry);
 				});
 				
-				console.info('[End Encoding]');
 			}
 			
 			DataManager.loadDatabase = DataManager_loadDatabase;
 			DataManager_loadDatabase.call(this);
 		};
 	}
-	
+
 })();
